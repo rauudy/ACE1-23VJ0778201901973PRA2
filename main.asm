a3baa7fa632559e1c2ba1ca131feefc7019f8fd5
RaudyCabrera <raudyc@gmail.com>
2023-06-29 19:28:14 -0600

    Mejoras

--- main.asm
+++ main.asm
@@ -3,8 +3,44 @@
 .STACK
 ;; PILA
 .DATA
+;;
+;;
+clave_capturada        db     09  dup (0)
+usuario_capturado      db     06  dup (0)
+espacio_leido          db     00
+estado                 db     00
+buffer_linea           db     0ff dup (0)
+tam_liena_leida        db     00
+handle_conf            dw     0000
+nombre_conf            db     "PRA2.CON", 00
+;;
+tam_encabezado_html    db     0c
+encabezado_html        db     "<html><body>"
+tam_inicializacion_tabla   db   3e
+inicializacion_tabla   db     '<table border="1"><tr><td>codigo</td><td>descripcion</td></tr>'
+tam_cierre_tabla       db     8
+cierre_tabla           db     "</table>"
+tam_footer_html        db     0e
+footer_html            db     "</body></html>"
+td_html                db     "<td>"
+tdc_html               db     "</td>"
+tr_html                db     "<tr>"
+trc_html               db     "</tr>"
+;; tokens
+tk_creds               db     0e, "[credenciales]"
+tk_nombre              db     07, "usuario"
+tk_clave               db     05, "clave"
+tk_igual               db     01, "="
+tk_comillas            db     01, '"'
+;;
+ceros          db     2b  dup (0)
 ;; VARIABLES | MEMORIA RAM
 numero           db   05 dup (30)
+;;
+nombre_usuario       db    "rau123",0a,"$"
+clave_usuario       db    "201901973",0a,"$"
+fallo       db    "Nel carnal",0a,"$"
+archivo_no_existente_msg db "El archivo de credenciales no existe",0a,"$"
 ;;
 usac       db    "Universidad de San Carlos de Guatemala",0a,"$"
 facultad   db    "Facultad de Ingenieria",0a,"$"
@@ -37,11 +73,15 @@ ingresar_prod    db  "(I)ngresar producto",0a,"$"
 editar_prod      db  "(E)ditar producto",0a,"$"
 borrar_prod      db  "(B)orrar producto",0a,"$"
 prods_registrados db "Productos registrados:",0a,"$"
+;;; temps
+cod_prod_temp    db    05 dup (0)
+puntero_temp     dw    0000
 ;; "ESTRUCTURA PRODUCTO"
 cod_prod    db    05 dup (0)
 cod_name    db    21 dup (0)
 cod_price   db    05 dup (0)
 cod_units   db    05 dup (0)
+;;
 ;; numéricos
 num_price   dw    0000
 num_units   dw    0000
@@ -49,21 +89,65 @@ num_units   dw    0000
 archivo_prods    db   "PROD.BIN",00
 handle_prods     dw   0000
 ;;
+nombre_rep1      db   "CATALG.HTM",00
+handle_reps      dw   0000
+;;
 .CODE
 .STARTUP
+
+print macro cadena
+    mov ah, 09h 
+    mov dx, offset cadena
+    int 21h
+endm
+
 ;; CODIGO
 inicio:
+mov ah, 3Dh         ; Código de función: Abrir archivo
+    mov al, 0           ; Modo de acceso de solo lectura
+    lea dx, nombre_conf    ; Dirección de la cadena de caracteres que contiene el nombre del archivo
+    int 21h             ; Llama a la interrupción 21h
+
+    jc archivo_no_existente   ; Salta a archivo_no_existente si hay un error al abrir el archivo
+
+    mov ah, 3Eh         ; Código de función: Cerrar archivo
+    mov bx, ax          ; Manejador de archivo devuelto por la función de apertura
+    int 21h             ; Llama a la interrupción 21h
+
+    ; mov ah, 9           ; Código de función: Escribir cadena de caracteres en la consola
+    ; lea dx, archivo_existente_msg   ; Dirección de la cadena de caracteres que indica que el archivo existe
+    ; int 21h             ; Llama a la interrupción 21h
+
+    jmp empezar
+
+archivo_no_existente:
+    mov ah, 9           ; Código de función: Escribir cadena de caracteres en la consola
+    lea dx, archivo_no_existente_msg   ; Dirección de la cadena de caracteres que indica que el archivo no existe
+    int 21h             ; Llama a la interrupción 21h
+
+
+empezar:
+		call validar_acceso
+		
+		mov CX, 0006
+    	mov SI, offset nombre_usuario
+    	mov DI, offset usuario_capturado
+    	inc DI
+    	call cadenas_iguales
+    	cmp DL, 0ff
+    	jne credencialesMal
+
+		mov CX, 0009
+		mov SI, offset clave_usuario
+		mov DI, offset clave_capturada
+		inc DI
+		call cadenas_iguales
+		cmp DL, 0ff
+		jne credencialesMal
+
+
+
 		;;
-		mov AX, 7e7
-		call numAcadena
-		mov BX, 01
-		mov CX, 0005
-		mov DX, offset numero
-		mov AH, 40
-		int 21
-		mov DX, offset nueva_lin
-		mov AH, 09
-		int 21
 		;; ENCABEZADO
 		mov DX, offset usac
 		mov AH, 09
@@ -81,6 +165,7 @@ inicio:
 		mov AH, 09
 		int 21
 		;;
+
 menu_principal:
 		mov DX, offset nueva_lin
 		mov AH, 09
@@ -113,7 +198,7 @@ menu_principal:
 		cmp AL, 76 ;; v minúscula ascii
 		je menu_ventas 
 		cmp AL, 68 ;; h minúscula ascii
-		je menu_herramientas 
+		je generar_catalogo 
 		jmp menu_principal
 menu_productos:
 		mov DX, offset nueva_lin
@@ -139,6 +224,7 @@ menu_productos:
 		int 21
 		;; AL = CARACTER LEIDO
 		cmp AL, 62 ;; borrar
+		je eliminar_producto_archivo
 		cmp AL, 65 ;; editar
 		cmp AL, 69 ;; insertar
 		je ingresar_producto_archivo
@@ -384,7 +470,7 @@ mostrar_productos_archivo:
 ciclo_mostrar:
 		;; puntero cierta posición
 		mov BX, [handle_prods]
-		mov CX, 26     ;; leer 26h bytes
+		mov CX, 0026     ;; leer 26h bytes
 		mov DX, offset cod_prod
 		;;
 		mov AH, 3f
@@ -397,14 +483,113 @@ ciclo_mostrar:
 		int 21
 		;; ¿cuántos bytes leímos?
 		;; si se leyeron 0 bytes entonces se terminó el archivo...
-		cmp AX, 00
+		cmp AX, 0000
 		je fin_mostrar
+		;; ver si es producto válido
+		mov AL, 00
+		cmp [cod_prod], AL
+		je ciclo_mostrar
 		;; producto en estructura
 		call imprimir_estructura
 		jmp ciclo_mostrar
 		;;
 fin_mostrar:
 		jmp menu_productos
+eliminar_producto_archivo:
+		mov DX, 0000
+		mov [puntero_temp], DX
+pedir_de_nuevo_codigo2:
+		mov DX, offset prompt_code
+		mov AH, 09
+		int 21
+		mov DX, offset buffer_entrada
+		mov AH, 0a
+		int 21
+		;;
+		mov DI, offset buffer_entrada
+		inc DI
+		mov AL, [DI]
+		cmp AL, 00
+		je  pedir_de_nuevo_codigo2
+		cmp AL, 05
+		jb  aceptar_tam_cod2  ;; jb --> jump if below
+		mov DX, offset nueva_lin
+		mov AH, 09
+		int 21
+		jmp pedir_de_nuevo_codigo2
+		;;; mover al campo codigo en la estructura producto
+aceptar_tam_cod2:
+		mov SI, offset cod_prod_temp
+		mov DI, offset buffer_entrada
+		inc DI
+		mov CH, 00
+		mov CL, [DI]
+		inc DI  ;; me posiciono en el contenido del buffer
+copiar_codigo2:	mov AL, [DI]
+		mov [SI], AL
+		inc SI
+		inc DI
+		loop copiar_codigo2  ;; restarle 1 a CX, verificar que CX no sea 0, si no es 0 va a la etiqueta, 
+		;;; la cadena ingresada en la estructura
+		;;;
+		mov DX, offset nueva_lin
+		mov AH, 09
+		int 21
+		;;
+		mov AL, 02              ;;;<<<<<  lectura/escritura
+		mov DX, offset archivo_prods
+		mov AH, 3d
+		int 21
+		mov [handle_prods], AX
+		;;; TODO: revisar si existe
+ciclo_encontrar:
+		mov BX, [handle_prods]
+		mov CX, 26
+		mov DX, offset cod_prod
+		moV AH, 3f
+		int 21
+		mov BX, [handle_prods]
+		mov CX, 4
+		mov DX, offset num_price
+		moV AH, 3f
+		int 21
+		cmp AX, 0000   ;; se acaba cuando el archivo se termina
+		je finalizar_borrar
+		mov DX, [puntero_temp]
+		add DX, 2a
+		mov [puntero_temp], DX
+		;;; verificar si es producto válido
+		mov AL, 00
+		cmp [cod_prod], AL
+		je ciclo_encontrar
+		;;; verificar el código
+		mov SI, offset cod_prod_temp
+		mov DI, offset cod_prod
+		mov CX, 0005
+		call cadenas_iguales
+		;;;; <<
+		cmp DL, 0ff
+		je borrar_encontrado
+		jmp ciclo_encontrar
+borrar_encontrado:
+		mov DX, [puntero_temp]
+		sub DX, 2a
+		mov CX, 0000
+		mov BX, [handle_prods]
+		mov AL, 00
+		mov AH, 42
+		int 21
+		;;; puntero posicionado
+		mov CX, 2a
+		mov DX, offset ceros
+		mov AH, 40
+		int 21
+finalizar_borrar:
+		mov BX, [handle_prods]
+		mov AH, 3e
+		int 21
+		jmp menu_productos
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 menu_ventas:
 		mov DX, offset titulo_ventas
 		mov AH, 09
@@ -421,6 +606,80 @@ menu_herramientas:
 		mov AH, 09
 		int 21
 		jmp fin
+generar_catalogo:
+		mov AH, 3c
+		mov CX, 0000
+		mov DX, offset nombre_rep1
+		int 21
+		mov [handle_reps], AX
+		mov BX, AX
+		mov AH, 40
+		mov CH, 00
+		mov CL, [tam_encabezado_html]
+		mov DX, offset encabezado_html
+		int 21
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, [tam_inicializacion_tabla]
+		mov DX, offset inicializacion_tabla
+		int 21
+		;;
+		mov AL, 02
+		mov AH, 3d
+		mov DX, offset archivo_prods
+		int 21
+		;;
+		mov [handle_prods], AX
+		;; leemos
+ciclo_mostrar_rep1:
+		;; puntero cierta posición
+		mov BX, [handle_prods]
+		mov CX, 26     ;; leer 26h bytes
+		mov DX, offset cod_prod
+		;;
+		mov AH, 3f
+		int 21
+		;; puntero avanzó
+		mov BX, [handle_prods]
+		mov CX, 0004
+		mov DX, offset num_price
+		mov AH, 3f
+		int 21
+		;; ¿cuántos bytes leímos?
+		;; si se leyeron 0 bytes entonces se terminó el archivo...
+		cmp AX, 00
+		je fin_mostrar
+		;; ver si es producto válido
+		mov AL, 00
+		cmp [cod_prod], AL
+		je ciclo_mostrar_rep1
+		;; producto en estructura
+		call imprimir_estructura_html
+		jmp ciclo_mostrar_rep1
+		;;
+fin_mostrar_rep1:
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, [tam_cierre_tabla]
+		mov DX, offset cierre_tabla
+		int 21
+		;;
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, [tam_footer_html]
+		mov DX, offset footer_html
+		int 21
+		;;
+		mov AH, 3e
+		int 21
+		jmp menu_principal
+
+
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; imprimir_estructura - ...
 ;; ENTRADAS:
@@ -459,6 +718,91 @@ poner_dolar_1:
 		mov AH, 09
 		int 21
 		ret
+
+;;; ENTRADA:
+;;    BX -> handle
+imprimir_estructura_html:
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, 04
+		mov DX, offset tr_html
+		int 21
+		;;
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, 04
+		mov DX, offset td_html
+		int 21
+		;;
+		mov DX, offset cod_prod
+		mov SI, 0000
+ciclo_escribir_codigo:
+		mov DI, DX
+		mov AL, [DI]
+		cmp AL, 00
+		je escribir_desc
+		cmp SI, 0006
+		je escribir_desc
+		mov CX, 0001
+		mov BX, [handle_reps]
+		mov AH, 40
+		int 21
+		inc DX
+		inc SI
+		jmp ciclo_escribir_codigo
+escribir_desc:
+		;;
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, 05
+		mov DX, offset tdc_html
+		int 21
+		;;
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, 04
+		mov DX, offset td_html
+		int 21
+		;;
+		mov DX, offset cod_name
+		mov SI, 0000
+ciclo_escribir_desc:
+		mov DI, DX
+		mov AL, [DI]
+		cmp AL, 00
+		je cerrar_tags
+		cmp SI, 0026
+		je cerrar_tags
+		mov CX, 0001
+		mov BX, [handle_reps]
+		mov AH, 40
+		int 21
+		inc DX
+		inc SI
+		jmp ciclo_escribir_desc
+		;;
+cerrar_tags:
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, 05
+		mov DX, offset tdc_html
+		int 21
+		;;
+		mov BX, [handle_reps]
+		mov AH, 40
+		mov CH, 00
+		mov CL, 05
+		mov DX, offset trc_html
+		int 21
+		;;
+		ret
+
+
 ;; cadenaAnum
 ;; ENTRADA:
 ;;    DI -> dirección a una cadena numérica
@@ -557,6 +901,222 @@ ciclo_memset:
 		loop ciclo_memset
 		ret
 
+;; cadenas_iguales -
+;; ENTRADA:
+;;    SI -> dirección a cadena 1
+;;    DI -> dirección a cadena 2
+;;    CX -> tamaño máximo
+;; SALIDA:
+;;    DL -> 00 si no son iguales
+
+;;         0ff si si lo son
+cadenas_iguales:
+ciclo_cadenas_iguales:
+		mov AL, [SI]
+		cmp [DI], AL
+		jne no_son_iguales
+		inc DI
+		inc SI
+		loop ciclo_cadenas_iguales
+		;;;;; <<<
+		mov DL, 0ff
+		ret
+no_son_iguales:	mov DL, 00
+		ret
+
+validar_acceso:
+		;; abrir archivo de configuración
+		mov AH, 3d
+		mov AL, 00
+		mov DX, offset nombre_conf
+		int 21
+		mov [handle_conf], AX
+		;; analizarlo
+		
+ciclo_lineaXlinea:
+		mov DI, offset buffer_linea
+		mov AL, 00
+		mov [tam_liena_leida], AL
+ciclo_obtener_linea:
+		mov AH, 3f
+		mov BX, [handle_conf]
+		mov CX, 0001
+		mov DX, DI
+		int 21
+		cmp CX, 0000
+		je fin_leer_linea
+		mov AL, [DI]
+		cmp AL, 0a
+		je fin_leer_linea
+		mov AL, [tam_liena_leida]
+		inc AL
+		mov [tam_liena_leida], AL
+		inc DI
+		jmp ciclo_obtener_linea
+fin_leer_linea:
+		mov AL, [tam_liena_leida]
+		mov AL, 00
+		cmp [estado], AL   ;; verificar la cadena credenciales
+		je verificar_cadena_credenciales
+		mov AL, 01
+		cmp [estado], AL   ;; obtener campo
+		je obtener_campo_conf
+		mov AL, 02
+		cmp [estado], AL   ;; obtener campo
+		je obtener_campo_conf
+		jmp retorno_exitoso
+verificar_cadena_credenciales:
+		cmp CX, 0000
+		je retorno_fallido
+		mov CH, 00
+		mov CL, [tk_creds]
+		mov SI, offset tk_creds
+		inc SI
+		mov DI, offset buffer_linea
+		call cadenas_iguales
+		cmp DL, 0ff
+		je si_hay_creds
+		jmp retorno_fallido
+si_hay_creds:
+		mov AL, [estado]
+		inc AL
+		mov [estado], AL
+		jmp ciclo_lineaXlinea
+		;;
+obtener_campo_conf:
+		cmp CX, 0000
+		je retorno_fallido
+		mov CH, 00
+		mov CL, [tk_nombre]
+		mov SI, offset tk_nombre
+		inc SI
+		mov DI, offset buffer_linea
+		call cadenas_iguales
+		cmp DL, 0ff
+		je obtener_valor_usuario
+		;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+		mov CH, 00
+		mov CL, [tk_clave]
+		mov SI, offset tk_clave
+		inc SI
+		mov DI, offset buffer_linea
+		call cadenas_iguales
+		cmp DL, 0ff
+		je obtener_valor_clave
+		jmp retorno_fallido
+obtener_valor_usuario:
+ciclo_espacios1:
+		inc DI
+		mov AL, [DI]
+		cmp AL, 20    ;; ver si es espacio
+		jne ver_si_es_igual
+		inc DI
+		jmp ciclo_espacios1
+ver_si_es_igual:
+		mov CH, 00
+		mov CL, [tk_igual]
+		mov SI, offset tk_igual
+		inc SI
+		call cadenas_iguales
+		cmp DL, 0ff
+		je obtener_valor_cadena_usuario
+		jmp retorno_fallido
+obtener_valor_cadena_usuario:
+ciclo_espacios2:
+		inc DI
+		mov AL, [DI]
+		cmp AL, 20    ;; ver si es espacio
+		jne capturar_usuario
+		inc DI
+		jmp ciclo_espacios2
+capturar_usuario:
+		mov CX, 0006    ;; TAMAÑO DEL USUARIO: 6 caracteres
+		mov SI, offset usuario_capturado
+ciclo_cap_usuario:
+		inc DI
+		inc SI
+		mov AL, [DI]
+		mov [SI], AL
+		loop ciclo_cap_usuario
+		mov AL, [estado]
+		inc AL
+		mov [estado], AL
+		jmp ciclo_lineaXlinea
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,
+obtener_valor_clave:
+ciclo_espacios3:
+		inc DI
+		mov AL, [DI]
+		cmp AL, 20    ;; ver si es espacio
+		jne ver_si_es_igual2
+		inc DI
+		jmp ciclo_espacios3
+ver_si_es_igual2:
+		mov CH, 00
+		mov CL, [tk_igual]
+		mov SI, offset tk_igual
+		inc SI
+		call cadenas_iguales
+		cmp DL, 0ff
+		je obtener_valor_cadena_clave
+		jmp retorno_fallido
+obtener_valor_cadena_clave:
+ciclo_espacios4:
+		inc DI
+		mov AL, [DI]
+		cmp AL, 20    ;; ver si es espacio
+		jne capturar_clave
+		inc DI
+		jmp ciclo_espacios4
+capturar_clave:
+		mov CX, 0009    ;; TAMAÑO DE LA CLAVE: 9 caracteres
+		mov SI, offset clave_capturada
+ciclo_cap_clave:
+		inc DI
+		inc SI
+		mov AL, [DI]
+		mov [SI], AL
+		loop ciclo_cap_clave
+		mov AL, [estado]
+		inc AL
+		mov [estado], AL
+		jmp ciclo_lineaXlinea
+		;; ver si el nombre de campo es "usuario"
+		;;      trabajo con la línea
+		;; comparar nombre
+		;; comparar clave
+		;; si son correctos devolver en DL = 0ff
+		;; si no son correctos devolver en DL = 00
+retorno_fallido:
+		mov DL, 00
+		ret
+retorno_exitoso:
+		mov DL, 0ff
+		ret
+
+; validarCredenciales:
+
+; 		call validar_acceso
+
+; 		mov CX, 0006
+;     	mov SI, offset nombre_usuario
+;     	mov DI, offset usuario_capturado
+;     	inc DI
+;     	call cadenas_iguales
+;     	cmp DL, 0ff
+;     	jne credencialesMal
+
+; 		mov CX, 0009
+; 		mov SI, offset clave_usuario
+; 		mov DI, offset clave_capturada
+; 		inc DI
+; 		call cadenas_iguales
+; 		cmp DL, 0ff
+; 		jne credencialesMal
+
+
+credencialesMal:
+	print fallo
 fin:
 .EXIT
 END
